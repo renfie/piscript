01a69ffcbb1c0c292fe66224bf899265bf273906
renfie <102348785+renfie@users.noreply.github.com>
2022-03-25 08:14:45 +0100

    Update uptime.sh

--- uptime.sh
+++ uptime.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 uptime=$(cat /proc/uptime | awk '// { printf "%dT %02d:%02d", $1/86400, $1/3600%24, $1/60%60 }')
 {
-echo From: renfieler@gmail.com
-echo To: renefieler@gmail.com
+echo From: xxx@gmail.com
+echo To: xxx@gmail.com
 echo Subject: Uptime
 echo
 echo $uptime
-} | sudo sendmail  renefieler@gmail.com
+} | sudo sendmail  xxx@gmail.com
