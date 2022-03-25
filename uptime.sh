1d7a8e5fb49207086960afcf656007f4ebb0dbfc
renfie <102348785+renfie@users.noreply.github.com>
2022-03-25 08:21:14 +0100

    Delete uptime.sh

--- uptime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-uptime=$(cat /proc/uptime | awk '// { printf "%dT %02d:%02d", $1/86400, $1/3600%24, $1/60%60 }')
-{
-echo From: xxx@gmail.com
-echo To: xxx@gmail.com
-echo Subject: Uptime
-echo
-echo $uptime
-} | sudo sendmail  xxx@gmail.com
