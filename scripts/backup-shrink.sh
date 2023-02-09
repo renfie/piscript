9183b660b9e9c6dc2fc62bd60275fd7140dbb05f
renfie <102348785+renfie@users.noreply.github.com>
2023-02-09 20:51:26 +0100

    Update backup-shrink.sh

--- scripts/backup-shrink.sh
+++ scripts/backup-shrink.sh
@@ -5,10 +5,11 @@ clear
 
 SMB_USER="xxx"
 SMB_PASS="xxx"
-SMB_DIR="//yout_path/Backup/pi"
-MOUNT_DIR="/local_mountdir"
+SMB_DIR="//xxxx/Backup/pi"
+MOUNT_DIR="/home/pi/mnt"
 
 sudo mount -t cifs -o vers=1.0,username=${SMB_USER},password=${SMB_PASS},rw,file_mode=0777,dir_mode=0777 ${SMB_DIR} ${MOUNT_DIR}
+sudo raspiBackup
 
 host=`hostname`
 mount="/home/pi/mnt/${host}/"
@@ -16,29 +17,16 @@ vts=$(date +'%Y-%m-%d')
 
 cd ${mount}
 mount2=$(ls -rt1 | tail -1)
-
 cd $mount2
 
 img=$(ls *.img -rt1 | tail -1)
-
 image=${mount}${mount2}"/"$img
 imagepath=${mount}${mount2}"/"
-
-
-raspibackup () {
-   sudo raspiBackup
-}
 
 pishrink () {
    pishrink.sh ${image}
 }
 
-makeall (){
-  raspibackup
-  pishrink
-}
-
-makeall
-#pishrink
+pishrink
 
 sudo umount $MOUNT_DIR
