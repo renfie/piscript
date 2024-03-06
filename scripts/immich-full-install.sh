6a2db50f3cf19373a2fa0a58bcf22268a0b5e2fc
renfie <102348785+renfie@users.noreply.github.com>
2024-03-06 08:55:01 +0100

    Create immich-full-install.sh

---
+++ scripts/immich-full-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+############################################
+#####Immich Full-Install                   #
+#####         © renfie 2024                #
+############################################
+
+echo "benötigte Ordner im Homeverzeichnis anlegen"
+cd ~
+mkdir $PWD/docker-files
+mkdir $PWD/docker-files/immich
+
+cd $PWD/docker-files/immich
+mkdir -p redis db upload micro cache
+
+echo "Update und Upgrade"
+apt update && apt dist-upgrade -y
+
+echo "curl installieren"
+apt install curl -y
+
+echo "hole get-docker"
+curl -fsSL https://get.Docker.com -o get-Docker.sh
+chmod +x get-Docker.sh
+
+echo "Docker installieren"
+./get-Docker.sh
+rm get-Docker.sh
+echo "notwendige Rechte anpassen"
+usermod -aG docker $USER
+
+echo "Portainer starten"
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer  --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data cr.portainer.io/portainer/portainer-ce:latest
+
+echo "hole passende docker-compose.yml"
+curl -fsSL https://raw.githubusercontent.com/renfie/piscript/main/scripts/immich.yml -o docker-compose.yml
+
+echo "starte immich-Container"
+docker compose up -d
