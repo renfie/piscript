9519c011f33dac971b7c8945046885ad805c546a
renfie <102348785+renfie@users.noreply.github.com>
2024-03-06 15:55:35 +0100

    Update immich-backup.sh

--- scripts/immich/immich-backup.sh
+++ scripts/immich/immich-backup.sh
@@ -23,3 +23,11 @@ ls -lh $MOUNT_DIR >> $THE_LOG
 # Garbagge Collection
 #rm $THE_LOG $THE_DUMP &
 find $MOUNT_DIR -name "*.gz" -daystart -mtime +14 -delete
+
+#Wiederherstellung der Datenbank
+#https://immich.app/docs/administration/backup-and-restore
+#Beachten Sie, dass für eine ordnungsgemäße Wiederherstellung der Datenbank eine vollständige Neuinstallation erforderlich ist 
+#(dh der Immich-Server wurde seit der Erstellung der Docker-Container nie ausgeführt). 
+#Wenn die Immich-App ausgeführt wurde, können bei der Datenbankwiederherstellung Postgres-Konflikte auftreten (Beziehung bereits vorhanden, verletzte Fremdschlüsseleinschränkungen, mehrere Primärschlüssel usw.).
+
+#gunzip < "/path/to/backup/dump.sql.gz" | docker exec -i immich_postgres psql -U postgres -d immich    # Restore Backup
