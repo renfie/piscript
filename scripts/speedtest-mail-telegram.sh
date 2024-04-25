672135b50856a3a84366263e9683d069a7da3ffc
RenFie <102348785+renfie@users.noreply.github.com>
2024-04-25 14:30:27 +0200

    Update speedtest-mail-telegram.sh

--- scripts/speedtest-mail-telegram.sh
+++ scripts/speedtest-mail-telegram.sh
@@ -3,14 +3,15 @@
 botID=XXX
 chatID=XXX
 mailAddress=bla@bla.com
+path=/home/pi/scripts
 
 test -f /usr/bin/speedtest || sudo apt install speedtest-cli -y
 
-speedtest --secure > /home/pi/scripts/temp.txt
-sed '1,6d' /home/pi/scripts/temp.txt > /home/pi/scripts/temp2.txt
-sed '2d' /home/pi/scripts/temp2.txt > /home/pi/scripts/temp.txt
+speedtest --secure > $path/temp.txt
+sed '1,6d' $path/temp.txt > $path/temp2.txt
+sed '2d' $path/temp2.txt > $path/temp.txt
 
-speedtest=$(cat /home/pi/scripts/temp.txt)
+speedtest=$(cat $path/temp.txt)
 
 #Ergebnis per Mail
 echo -e "Subject:Speedtest\n\n $speedtest \n\n" | sudo sendmail $mailAddress
@@ -19,5 +20,5 @@ echo -e "Subject:Speedtest\n\n $speedtest \n\n" | sudo sendmail $mailAddress
 curl -X POST "https://api.telegram.org/$botID/sendMessage" -d "chat_id=$chatID&text=$speedtest"
 
 #garbage collection
-rm /home/pi/scripts/temp.txt
-rm /home/pi/scripts/temp2.txt
+rm $path/temp.txt
+rm $path/temp2.txt
