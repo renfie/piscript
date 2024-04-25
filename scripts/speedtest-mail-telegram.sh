0d3d13628e6dcc60aabba025e4fa4ae7f6c9622f
RenFie <102348785+renfie@users.noreply.github.com>
2024-04-25 14:23:30 +0200

    Update speedtest-mail-telegram.sh

--- scripts/speedtest-mail-telegram.sh
+++ scripts/speedtest-mail-telegram.sh
@@ -3,6 +3,8 @@
 botID=XXX
 chatID=XXX
 
+test -f /usr/bin/speedtest || sudo apt install speedtest-cli -y
+
 speedtest --secure > /home/pi/scripts/temp.txt
 sed '1,6d' /home/pi/scripts/temp.txt > /home/pi/scripts/temp2.txt
 sed '2d' /home/pi/scripts/temp2.txt > /home/pi/scripts/temp.txt
@@ -14,5 +16,7 @@ echo -e "Subject:Speedtest-Garten\n\n $speedtest \n\n" | sudo sendmail renefiele
 
 #Ergebnis per Telegram
 curl -X POST "https://api.telegram.org/$botID/sendMessage" -d "chat_id=$chatID&text=$speedtest"
+
+#garbage collection
 rm /home/pi/scripts/temp.txt
 rm /home/pi/scripts/temp2.txt
