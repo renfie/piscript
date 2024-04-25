dd2e83652b62223be9ebaaa15935483b0fbe437b
RenFie <102348785+renfie@users.noreply.github.com>
2024-04-25 09:16:52 +0200

    Create speedtest-mail-telegram.sh

---
+++ scripts/speedtest-mail-telegram.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+botID=XXX
+chatID=XXX
+
+speedtest --secure > /home/pi/scripts/temp.txt
+sed '1,6d' /home/pi/scripts/temp.txt > /home/pi/scripts/temp2.txt
+sed '2d' /home/pi/scripts/temp2.txt > /home/pi/scripts/temp.txt
+
+speedtest=$(cat /home/pi/scripts/temp.txt)
+
+#Ergebnis per Mail
+echo -e "Subject:Speedtest-Garten\n\n $speedtest \n\n" | sudo sendmail renefieler@gmail.com
+
+#Ergebnis per Telegram
+curl -X POST "https://api.telegram.org/$botID/sendMessage" -d "chat_id=$chatID&text=$speedtest"
+rm /home/pi/scripts/temp.txt
+rm /home/pi/scripts/temp2.txt
